e96649e9372da19b42e3b286100a59cc45a0b747
Jonas Silva <jonas86@edu.unisinos.br>
2023-09-20 18:26:42 -0300

    [update]

--- api/server.js
+++ api/server.js
@@ -13,6 +13,108 @@ server.use(jsonServer.rewriter({
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
+
+ server.get("/", (req, res) => {
+     res.send("Hello, World!!!");
+ });
+
+ server.post("/users", (req, res) => {
+    const { email, username, password, address, cpf, hydrometer, isAdmin,  } = req.body;
+
+    // Verifique se o usuário já existe no banco de dados
+    const existingUser = jsonServerMiddleware.db
+        .get("users")
+        .find({ username })
+        .value();
+
+    if (existingUser) {
+        return res
+            .status(400)
+            .json({ success: false, message: "Usuário já existe" });
+    }
+
+    // Obtenha o último ID de usuário no banco de dados
+    const lastUser = jsonServerMiddleware.db
+        .get("users")
+        .orderBy("id", "desc")
+        .first()
+        .value();
+
+    // Calcule o próximo ID disponível
+    const nextId = lastUser ? lastUser.id + 1 : 1;
+
+    // Adicione o novo usuário com o próximo ID
+    jsonServerMiddleware.db
+        .get("users")
+        .push({ id: nextId, email, username, password, address, cpf, hydrometer, paid: false, isAdmin })
+        .write();
+
+    res.json({ success: true, message: "Usuário adicionado com sucesso" });
+});
+
+server.patch("/users/:id/pay", (req, res) => {
+    const userId = parseInt(req.params.id);
+    const user = jsonServerMiddleware.db
+        .get("users")
+        .find({ id: userId })
+        .value();
+
+    if (!user) {
+        return res
+            .status(404)
+            .json({ success: false, message: "Usuário não encontrado" });
+    }
+
+    // Atualize a chave 'paid' para true
+    jsonServerMiddleware.db
+        .get("users")
+        .find({ id: userId })
+        .assign({ paid: true })
+        .write();
+
+    res.json({
+        success: true,
+        message: "Status de pagamento atualizado para true",
+    });
+});
+
+server.post("/users/login", (req, res) => {
+    const { username, password } = req.body;
+
+    const users = jsonServerMiddleware.db.get("users").value();
+    const user = users.find(
+        (u) => u.username === username && u.password === password
+    );
+
+    if (user) {
+        res.json({
+            success: true,
+            message: "Login bem-sucedido",
+            userInfo: {
+                id: user.id,
+                email: user.email,
+                username: user.username,
+                address: user.address,
+                CPF: user.CPF,
+                hydrometer: user.hydrometer,
+                paid: user.paid,
+                isAdmin: user.isAdmin,
+            },
+        });
+    } else {
+        res.status(401).json({
+            success: false,
+            message: "Credenciais inválidas",
+        });
+    }
+});
+
+// Use o JSON Server apenas para as rotas padrão
+server.use(jsonServerMiddleware);
+
+server.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
 })
 
 // Export the Server API
